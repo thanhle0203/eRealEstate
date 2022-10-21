2ca9c2c98cb7c29a01a2bed496c03c6b9a13ecde
Thanh Le <thanhlevan0203@gmail.com>
2022-10-20 23:08:41 -0700

    Installed indepencies

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+
+
+export default function Home() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
