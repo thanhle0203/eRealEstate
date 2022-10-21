b31ef8345d0d58576d643dcced7f1871dc6411bf
Thanh Le <thanhlevan0203@gmail.com>
2022-10-20 23:14:14 -0700

    Create Banner

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,21 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-
+const Banner = ({ purpose }) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box>
+      
+    </Box>
+  </Flex>
+)
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <di>
+      <h1>Hello World</h1>
+      <Banner purpose={'For Sale'} />
+      <Banner purpose={'For Rent'} />
+    </di>
+    
   )
 }
