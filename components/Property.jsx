b1f7464f0c7166840236e6994e3a7c4ccea69d3d
Thanh Le <thanhlevan0203@gmail.com>
2022-10-21 00:30:36 -0700

    Create Property Component

---
+++ components/Property.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/goverified';
+import millify from 'millify';
+
+const Property = ({ property: { converPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId }}) => (
+  <Link href={`/property/${externalID}`} passHref>
+    {title}
+  </Link>
+)
+
+export default Property;
