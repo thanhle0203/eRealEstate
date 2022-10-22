d86e9709d698e0433c3d573d37f6e5dffa57c2a2
Thanh Le <thanhlevan0203@gmail.com>
2022-10-22 00:29:19 -0700

    Fetch Logo, Image, Price, Number Bed & Bad, Title of Rent & Sale

--- components/Property.jsx
+++ components/Property.jsx
@@ -18,9 +18,18 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
         <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
           <Flex alignItems="center">
             <Box paddingRight="3" color="green.400">{isVerified && <GoVerified />}</Box>
-            <Text fontWeight="bold" fontSize="lg">USD {price}{rentFrequency && `/${rentFrequency}`}</Text>
+            <Text fontWeight="bold" fontSize="lg">USD {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
           </Flex>
+          <Box>
+            <Avatar size="sm" src={agency?.logo?.url} />
+          </Box>
         </Flex>
+        <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqrt <BsGridFill />
+        </Flex>
+        <Text fontSize="lg">
+          {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+        </Text>
       </Box>
     </Flex>
   </Link>
