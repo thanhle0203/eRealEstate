893d1245e63f3c02fb66ff401cedb8eb8b7f6191
Thanh Le <thanhlevan0203@gmail.com>
2022-10-22 22:23:15 -0700

    Create Search Page

---
+++ pages/Search.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { Flex, Box, Text, Icon } from '@chakra-ui/react';
+import { BsFilter } from "react-icons/bs";
+
+const Search = () => {
+  const [searchFilter, setSearchFilters] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box>
+      <Flex
+        cursor="pointer"
+        bg="gray.100"
+        borderBottom="1px"
+        borderColor="gray.200"
+        p="2"
+        fontWeight="black"
+        fontSize="lg"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>Search Property By Filters</Text>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Search;
