da815ee21e81f70f27d70e0662efbc6abf3682a1
Thanh Le <thanhlevan0203@gmail.com>
2022-10-23 22:53:49 -0700

    Seach Properties by Filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,12 +3,26 @@ import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-u
 import { useRouter } from "next/router";
 import { MdCancel } from 'react-icons/md';
 import Image from "next/image";
+
 import { filterData, getFilterValues } from '../utils/filterData';
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+import noresult from '../assets/images/noresult.svg';
 
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
+  const router = useRouter();
 
   const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      query[item.name] = item.value
+    })
+
+    router.push({ pathname: path, query })
 
   }
 
