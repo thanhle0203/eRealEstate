d216c132d1531c9b3528746856d98b3f0d475f73
Thanh Le <thanhlevan0203@gmail.com>
2022-10-24 23:15:13 -0700

    Fetch Image to ImageScrollbar

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -38,8 +38,22 @@ const RightArrow = () => {
     )
   }
 
-const ImageScrollbar = () => (
+const ImageScrollbar = ({ data }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow: 'hidden'}}>
-
+    {data.map((item) => (
+      <Box key={item.id} width="910px" itemId={item.id} overflow="hidden" padding="1">
+        <Image 
+          alt="property"
+          placeholder="blur"
+          blurDataURL={item.url}
+          src={item.url}
+          width={1000}
+          height={500}
+          sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
+         />
+      </Box>
+    ))}
   </ScrollMenu>
 )
+
+export default ImageScrollbar;
