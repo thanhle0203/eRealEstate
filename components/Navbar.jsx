e1f8d4e5abb7ca3ae6a9f3254873e1b9f7f5d6f4
Thanh Le <thanhlevan0203@gmail.com>
2022-10-25 22:04:43 -0700

    new  updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,13 +6,13 @@ import { FiKey } from 'react-icons/fi';
 
 const Navbar = () => (
   <Flex p="2" borderBottom="1px" borderColor="gray.100">
-    <Box frontSize="3xl" color="blue.400" fontWeight="bold">
+    <Box frontSize="5xl" color="red.400" fontWeight="bold">
       <Link href='/' paddingLeft='2'>eRealEstate</Link>
     </Box>
     <Spacer />
     <Box>
       <Menu>
-        <MenuButton as={IconButton} icon={<FcMenu />} variant="outline" color="red.400" />
+        <MenuButton as={IconButton} icon={<FcMenu />} variant="outline" color="red.300" />
         <MenuList>
           <Link href="/" passHref>
             <MenuItem icon={<FcHome />}>Home</MenuItem>
